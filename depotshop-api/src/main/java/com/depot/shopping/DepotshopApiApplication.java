369aab07955112144db3d6428d0bd30e9132f5dc
ubd2000 <love_alcls@naver.com>
2024-06-23 23:48:17 +0900

    Add : 1차 Multi Module 설계

---
+++ depotshop-api/src/main/java/com/depot/shopping/DepotshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.depot.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepotshopApiApplication.class, args);
+	}
+
+}
---
+++ depotshop-api/src/test/java/com/depot/shopping/DepotshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depot.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepotshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
