564439d2978cce2668336b204ecb1100753e3e8f
ubd2000 <love_alcls@naver.com>
2024-06-24 23:55:24 +0900

    Feat : batch module 추가

---
+++ depotshop-batch/src/main/java/com/depot/shopping/DepotshopBatchApplication.java
@@ -0,0 +1,13 @@
+package com.depot.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotshopBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepotshopBatchApplication.class, args);
+	}
+
+}
---
+++ depotshop-batch/src/test/java/com/depot/shopping/DepotshopBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depot.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepotshopBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
