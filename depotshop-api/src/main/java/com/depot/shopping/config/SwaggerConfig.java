8a2f60921d274555590e3ce4a99b720dd3aff048
kimdm1028 <kimdm1028@btsoft.kr>
2024-06-24 18:48:50 +0900

    Feat : Swagger 추가 및 API Health Check 추가

---
+++ depotshop-api/src/main/java/com/depot/shopping/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.depot.shopping.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kdm
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Shopping API")
+                .version("1.0")
+                .description("Shopping API Documentation");
+    }
+}
