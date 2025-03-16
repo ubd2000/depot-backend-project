d8e844156a877736e6476235ea0a43fdacfe00ad
DongMin kim <dongminkim5067@gmail.com>
2025-03-16 23:20:18 +0900

    Fix : @GeneratedValue 수정

--- depotshop-domain/src/main/java/com/depot/shopping/domain/user/entity/Users.java
+++ depotshop-domain/src/main/java/com/depot/shopping/domain/user/entity/Users.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class Users {
 
   @Id
+  @GeneratedValue(generator = "users_seq")
   @Column(name = "seq_id")
   private long seqId;
   @Column(name = "user_id")
