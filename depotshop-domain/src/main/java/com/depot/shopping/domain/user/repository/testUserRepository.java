4f5b103c34bd7d90ac5c7bff45adf3fe5ab1727a
jhshin <hddkkll@easymedia.net>
2025-03-17 14:12:08 +0900

    Repository 어노테이션 추가

--- depotshop-domain/src/main/java/com/depot/shopping/domain/user/repository/testUserRepository.java
+++ depotshop-domain/src/main/java/com/depot/shopping/domain/user/repository/testUserRepository.java
@@ -2,10 +2,12 @@ package com.depot.shopping.domain.user.repository;
 
 import com.depot.shopping.domain.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * 사용자 조회(임시)
  */
+@Repository
 public interface testUserRepository extends JpaRepository<Users, String> {
     Users findByUserId(String userId);
 }
